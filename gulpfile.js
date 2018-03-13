5812c4ad642fb27b0a2e305222d1e4ceeab83b1c
Ray Villalobos <ray@planetoftheweb.com>
2018-03-13 09:31:26 -0400

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp'),
+webserver = require('gulp-webserver');
+
+var builds = 'app';
+
+gulp.task('js', function() {
+return gulp.src(builds + '/js/myscript.js')
+});
+
+gulp.task('html', function() {
+gulp.src(builds + '/**/*.html');
+});
+
+gulp.task('css', function() {
+gulp.src(builds + '/**/*.css');
+});
+
+gulp.task('watch', function() {
+gulp.watch(builds + '/**/*.js', ['js']);
+gulp.watch(builds + '/**/*.css', ['css']);
+gulp.watch([builds + '/**/*.html'], ['html']);
+});
+
+gulp.task('webserver', function() {
+gulp.src(builds + '/')
+    .pipe(webserver({
+        port: 3322,
+        livereload: true,
+        livereloadport: 8283,
+        open: true
+    }));
+});
+
+gulp.task('default', ['watch', 'html', 'js', 'css', 'webserver']);
