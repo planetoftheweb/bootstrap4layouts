be24a375609779e748d08f1bbf9d8abb148ca95e
Ray Villalobos <ray@planetoftheweb.com>
2018-03-20 16:29:00 -0400

    added carousel

--- docs/js/script.js
+++ docs/js/script.js
@@ -20,10 +20,18 @@ $(function() {
       .attr('src', $(event.relatedTarget).data('highres'));
   });
 
+  //replace IMG inside carousels with a background image
+  $('#my-carousel .fade .carousel-item img').each(function() {
+    var imgSrc = $(this).attr('src');
+    $(this).parent().css({'background-image': 'url('+imgSrc+')'});
+    $(this).remove();
+  });
+
+
   //Use smooth scrolling when clicking on navigation
   $('.navbar-nav a:not(.dropdown-toggle)').click(function() {
-    if (location.pathname.replace(/^\//,'') === 
-      this.pathname.replace(/^\//,'') && 
+    if (location.pathname.replace(/^\//,'') ===
+      this.pathname.replace(/^\//,'') &&
       location.hostname === this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
