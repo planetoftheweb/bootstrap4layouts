00bc46cd112d6fa3bd4bf83e36db7ad15f713357
Ray Villalobos <ray@planetoftheweb.com>
2019-10-10 19:48:16 -0400

    created a src and build folders, new gulp process which handles sass and babel for handling newer versions of js

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,56 @@
-const {src, dest, series, parallel, watch} = require('gulp');
-const browserSync = require('browser-sync').create();
+const { src, dest, series, parallel, watch } = require('gulp')
+const del = require('del')
+const browserSync = require('browser-sync').create()
+const sass = require('gulp-sass')
 
-const babel= require('gulp-babel');
+const babel = require('gulp-babel')
 
-const origin = 'public';
-const destination = 'public';
+const origin = 'src'
+const destination = 'build'
 
+sass.compiler = require('node-sass')
+
+async function clean(cb) {
+  await del(destination)
+  cb()
+}
 
 function html(cb) {
-  cb();
+  src(`${origin}/**/*.html`).pipe(dest(destination))
+  cb()
 }
 
 function css(cb) {
-  cb();
+  src(`${origin}/css/style.scss`)
+    .pipe(
+      sass({
+        outputStyle: 'compressed'
+      })
+    )
+
+    .pipe(dest(`${destination}/css`))
+
+  cb()
 }
 
 function js(cb) {
-  cb();
+  src(`${origin}/js/lib/**/*.js`).pipe(dest(`${destination}/js/lib`))
+
+  src(`${origin}/js/script.js`)
+    .pipe(
+      babel({
+        presets: ['@babel/env']
+      })
+    )
+    .pipe(dest(`${destination}/js`))
+  cb()
 }
 
 function watcher(cb) {
   watch(`${origin}/**/*.html`).on('change', series(html, browserSync.reload))
   watch(`${origin}/**/*.scss`).on('change', series(css, browserSync.reload))
   watch(`${origin}/**/*.js`).on('change', series(js, browserSync.reload))
-  cb();
+  cb()
 }
 
 function server(cb) {
@@ -32,9 +59,9 @@ function server(cb) {
     open: true,
     server: {
       baseDir: destination
-    }   
+    }
   })
-  cb();
+  cb()
 }
 
-exports.default = series(parallel(html, css, js), server, watcher);
+exports.default = series(clean, parallel(html, css, js), server, watcher)
